14ea05a3d4ed69dada9e15240981df9df8aa8a16
kaminora <kaminoura9207@gmail.com>
2019-09-22 17:31:28 +0900

    Add script to get only some characters

--- getUCN.sh
+++ getUCN.sh
@@ -14,8 +14,14 @@ else
 fi
 
 # Get values of code for all characters
-xmllint --xpath "/ttFont/cmap/cmap_format_4/map/@code" $1 |
-  sed -e "s/code=\"\([^\"]*\)\"/\1\n/g" | while read line
+# xmllint --xpath "/ttFont/cmap/cmap_format_4/map/@code" $1 |
+  # sed -e "s/code=\"\([^\"]*\)\"/\1\n/g" | while read line
+# do
+  # echo $line | sed -e "s/0x/\\\u/"
+# done
+
+# Get values of code for hiragana, katakana, CJK
+cat $1 | grep -e HIRAGANA -e KATAKANA -e CJK | sed -e "s/.*code=\"\([^\"]*\)\".*/\1/g" | while read line
 do
-  echo $line | sed -e "s/0x/\\\u/"
+  echo $line | sed -e "s/0x/\\\u/" >> output.txt
 done
