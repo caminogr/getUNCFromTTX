4193bb5de2bfe0c442d10dacbcaf8d999c5dfcbd
kaminora <kaminoura9207@gmail.com>
2019-09-22 17:15:17 +0900

    Get values of code property

---
+++ getUCN.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if sed --version 2>/dev/null | grep -q GNU; then
+  echo Use sed
+else
+  if type "gsed" > /dev/null 2>&1; then
+    echo Use gsed
+    alias sed='gsed'
+  else
+    echo You need to install gsed
+    echo Exit
+    exit 1
+  fi
+fi
+
+# Get values of code for all characters
+xmllint --xpath "/ttFont/cmap/cmap_format_4/map/@code" $1 |
+  sed -e "s/code=\"\([^\"]*\)\"/\1\n/g" | while read line
+do
+  echo $line | sed -e "s/0x/\\\u/"
+done
